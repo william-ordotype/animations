0d8afe3cb4c0fcedb31a6e9ec179891ebbed9e85
Raquel Lopez <raquelcle@gmail.com>
2023-08-30 11:20:54 -0400

    Switch name and lastName order

--- src/components/DocumentsDrawer.js
+++ src/components/DocumentsDrawer.js
@@ -91,7 +91,7 @@ function DocumentsDrawer() {
         return {
           ["x-text"]:
             // "($store.documentsStore.getOne.document.member.lastName === undefined ? '' : $store.documentsStore.getOne.document.member.lastName) + ' ' + ($store.documentsStore.getOne.document.member.name === 'undefined' ? $store.documentsStore.getOne.document.member.name : 'Non renseigné')",
-            "($store.documentsStore.getOne.document.member.lastName === undefined && $store.documentsStore.getOne.document.member.name === undefined) ? 'Non renseigné' : ($store.documentsStore.getOne.document.member.lastName || '') + ' ' + ($store.documentsStore.getOne.document.member.name || '')",
+            "($store.documentsStore.getOne.document.member.lastName === undefined && $store.documentsStore.getOne.document.member.name === undefined) ? 'Non renseigné' : ($store.documentsStore.getOne.document.member.name || 'Non renseigné') + ' ' + ($store.documentsStore.getOne.document.member.lastName || 'Non renseigné')",
         };
       },
     };
