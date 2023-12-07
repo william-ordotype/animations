8bdba70b9523c5071c4f48d2d1d79381e8a0597d
Raquel Lopez <raquelcle@gmail.com>
2023-12-06 22:13:55 -0400

    Add sharing and cloning notes functionality in datatable and drawer
    In order to make the applications notes sharing and cloning notes possible, changes have been made to multiple files. In 'noteSharesValidation.js' and 'notesValidation.js', 'updated_by.full_name' is now being validated. Sharing functionality is implemented inside 'DocumentsDrawer.js'. New import statements have been added for 'SkeletonLoaderEvent' and 'ShareNotesService', also there are several new methods to handle sharing and interaction with shared notes. The return statement is added to 'setRemoveSharedInvitations' function in 'sharedNotesActions.js' to enhance control flow.

--- src/validation/noteSharesValidation.js
+++ src/validation/noteSharesValidation.js
@@ -7,6 +7,7 @@ const sortByValues = [
   "title",
   "type",
   "prescription_type",
+  "updated_by.full_name",
 ];
 const activateSharedNoteValidation = async (payload) => {
   const activateSharedNoteSchema = object();
