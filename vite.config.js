967af96bfc883aae91868de556ff9250d4a3c5a9
Raquel Lopez <raquelcle@gmail.com>
2024-04-14 23:08:00 -0400

    Add page shortcuts in terminal after running dev server
    Signed-off-by: Raquel Lopez <raquelcle@gmail.com>

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,44 @@
 import { defineConfig } from "vite";
 import { resolve } from "path";
 
+const showPagesUrlPlugin = () => {
+  let config;
+
+  return {
+    name: "read-config",
+    /**
+     * @param {import("vite").ResolvedConfig} resolvedConfig
+     */
+    configResolved(resolvedConfig) {
+      // store the resolved config
+      config = resolvedConfig;
+
+      if (config.command === "serve") {
+        // dev: plugin invoked by dev server
+        const baseUrl = `http://localhost:${config.server.port}`;
+        const pageRoot = "src/pages";
+
+        const myNotesUrl = `${baseUrl}/${pageRoot}/my-notes/`;
+        const notesSharedWithMe = `${baseUrl}/${pageRoot}/my-shared-documents/`;
+        const notesSharingInvite = `${baseUrl}/${pageRoot}/document-shared-invite/`;
+        const pathologiesUrl = `${baseUrl}/${pageRoot}/pathologies/`;
+
+        console.group("Pages Urls");
+        console.log(`- My notes: ${myNotesUrl}`);
+        console.log(`- Notes Shared with me: ${notesSharedWithMe}`);
+        console.log(`- Shared note invitation: ${notesSharingInvite}`);
+        console.log(`- Pathologies: ${pathologiesUrl}`);
+        console.groupEnd();
+      }
+    },
+  };
+};
+
 export default defineConfig({
   server: {
     port: 3021,
   },
+  plugins: [showPagesUrlPlugin()],
   resolve: {
     alias: {
       "@assets": resolve(__dirname, "./src/assets"),
