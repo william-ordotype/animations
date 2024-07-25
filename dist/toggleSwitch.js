c5fcdf755d8f56ffe993c60fd16b2f9579868990
Raquel Lopez <raquelcle@gmail.com>
2024-07-24 21:02:41 -0400

    style fix toggle switch

--- dist/toggleSwitch.js
+++ dist/toggleSwitch.js
@@ -6,10 +6,10 @@ function e(){var t=document.createElement("style");t.innerHTML=`
         [x-ordo-utils=slider]:before {
           position: absolute;
           content: "";
-          height: 26px;
-          width: 26px;
-          left: 4px;
-          bottom: 4px;
+          height: 16px;
+          width: 16px;
+          left: 2px;
+          bottom: 2px;
           background-color: white;
           -webkit-transition: .4s;
           transition: .4s;
@@ -25,8 +25,8 @@ function e(){var t=document.createElement("style");t.innerHTML=`
         }
         
         input:checked + [x-ordo-utils=slider]:before {
-          -webkit-transform: translateX(26px);
-          -ms-transform: translateX(26px);
-          transform: translateX(26px);
+          -webkit-transform: translateX(16px);
+          -ms-transform: translateX(16px);
+          transform: translateX(16px);
         }
     `,document.head.appendChild(t)}function o(){e()}o();
