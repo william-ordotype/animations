14b0ab8860b33f89877f383236d7e7d5ed9f16a4
Raquel Lopez <raquelcle@gmail.com>
2024-07-24 22:17:16 -0400

    build: create cookies manager

---
+++ src/cookiesManager.js
@@ -0,0 +1,31 @@
+'use strict';
+
+function toggleCookiesManager() {
+    const cookieManagerButton = document.querySelector('[x-ordo-utils="cookieManagerButton"]');
+    const cookieManagerClose = document.querySelector('[x-ordo-utils="cookieManagerClose"]');
+
+    // Open manager
+    cookieManagerButton.addEventListener('click', (e) => {
+        $(cookieManagerButton)
+            .css({
+                display: 'block',
+                opacity: 1,
+                bottom: '0'
+            }).animate({
+            opacity: 0,
+            bottom: '-100%'
+        }, 800);
+    });
+
+    // Close manager
+    cookieManagerClose.addEventListener('click', (e) => {
+        $(cookieManagerButton).css({
+            display: 'block',
+            opacity: 0,
+            bottom: '-100%'
+        }).animate({
+            opacity: 1,
+            bottom: '0'
+        }, 400);
+    })
+}
